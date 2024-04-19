2b79f9fe98d2e22556b2f08ba03df55fbb7bdcd2
Davi Gledson <davigledson@outlook.com>
2024-04-19 17:37:49 -0300

    alterando critérios de ordenação

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.order_by('created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
